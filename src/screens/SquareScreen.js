d9ce9e24fd7b1af4db22e4c7983eb9cedf7fb370
rinalfauzi <rinalfauzi@gmail.com>
2020-09-02 21:46:45 +0700

    Reuseable State

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,22 +8,67 @@ const SquareScreen = () => {
     const [green, setGreen] = useState(0);
 
     const COLOR_INCREMENT = 15
+
+    const setColor = (color, change) => {
+        //color === `red`, `green`, `blue`
+        //change === +15, -15
+
+        switch (color){
+            case 'red':
+                //Versi 1
+                    red + change > 255 || red + change < 0 ? null : setRed(red + change)
+                    return;
+                //Versi 2
+                    // if (red + change > 255 || red + change < 0){
+                    //     return;
+                    // } else {
+                    //     setRed(red + change)
+                    // }
+            case 'green':
+                //Versi 1
+                    green + change > 255 || green + change < 0 
+                    ? null 
+                    : setGreen(green + change)
+                    return;
+                //Versi 2
+                    // if (red + change > 255 || red + change < 0){
+                    //     return;
+                    // } else {
+                    //     setRed(red + change)
+                    // }
+            case 'blue':
+                //Versi 1
+                    blue + change > 255 || blue + change < 0 
+                    ? null 
+                    : setBlue(blue + change)
+                    return;
+                //Versi 2
+                    // if (red + change > 255 || red + change < 0){
+                    //     return;
+                    // } else {
+                    //     setRed(red + change)
+                    // }
+            default:
+                return;
+
+        }
+    }
     
     return (
         <View>
             <ColorCounter 
-                onIncrease={() => setRed(red + COLOR_INCREMENT)}
-                onDecrease= {() => setRed(red - COLOR_INCREMENT)}
+                onIncrease={() => setColor('red', COLOR_INCREMENT)}
+                onDecrease= {() => setColor('red', -1 * COLOR_INCREMENT)}
                 color="Red"
             />
             <ColorCounter 
-                onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-                onDecrease= {() => setGreen(green - COLOR_INCREMENT)}
+                onIncrease={() => setColor('green', COLOR_INCREMENT)}
+                onDecrease= {() => setColor('green', -1 * COLOR_INCREMENT)}
                 color="Green"
             />
             <ColorCounter
-                onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-                onDecrease= {() => setBlue(blue - COLOR_INCREMENT)}
+                onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+                onDecrease= {() => setColor('blue', -1 * COLOR_INCREMENT)}
                 color="Blue"
             />
             <View 
