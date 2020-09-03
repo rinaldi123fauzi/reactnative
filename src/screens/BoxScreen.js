39daa84d0c41f498465af62702542e690762245d
rinalfauzi <rinalfauzi@gmail.com>
2020-09-03 15:05:19 +0700

    Justify Content (Layouts)

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -15,7 +15,9 @@ const styles = StyleSheet.create({
     viewStyle:{
         borderWidth: 3,
         borderColor: 'black',
-        alignItems: 'center'
+        flexDirection: 'row',
+        height: 200,
+        justifyContent: 'flex-start'
     },
     textStyle:{
         borderWidth: 1,
