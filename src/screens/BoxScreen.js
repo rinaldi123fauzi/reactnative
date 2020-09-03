b8eff6a26cc96dd524cbc0a9603c68d9ff91ffa6
rinalfauzi <rinalfauzi@gmail.com>
2020-09-03 20:43:19 +0700

    Flex Value (Layouts)

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,9 +4,9 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () =>{
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Child #1</Text>
-            <Text style={styles.textStyle}>Child #2</Text>
-            <Text style={styles.textStyle}>Child #3</Text>
+            <Text style={styles.textOneStyle}>Child #1</Text>
+            <Text style={styles.textTwoStyle}>Child #2</Text>
+            <Text style={styles.textThreeStyle}>Child #3</Text>
         </View>
     )
 }
@@ -15,12 +15,23 @@ const styles = StyleSheet.create({
     viewStyle:{
         borderWidth: 3,
         borderColor: 'black',
-        flexDirection: 'row',
-        height: 200,
-        justifyContent: 'flex-start'
+        height: 200
     },
     textStyle:{
         borderWidth: 1,
+        borderColor: 'red'
+    },
+    textOneStyle:{
+        borderWidth: 3,
+        borderColor: 'red'
+    },
+    textTwoStyle:{
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 1
+    },
+    textThreeStyle:{
+        borderWidth: 3,
         borderColor: 'red'
     }
 
